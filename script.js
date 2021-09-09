ae135b5c2659e024e6cfbc82de6b37e5a414c2bf
Tarun-maalaukset <90245031+Tarun-maalaukset@users.noreply.github.com>
2021-09-09 17:48:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,20 @@
-var fields = {};
-document.addEventListener("DOMContentLoaded", function() {
-    fields.firstName = document.getElementById('firstName');
-    fields.lastName = document.getElementById('lastName');
-    fields.email = document.getElementById('email');
-    fields.address = document.getElementById('address');
-    fields.houseNumber = document.getElementById('houseNumber');
-    fields.country = doucment.getElementById('country');
-    fields.question = document.getElementById('question');
-})
+// Get the modal
+var modal = document.getElementById('myModal');
 
-function isNotEmpty(value) {
-    if (value == null || typeof value == 'undefined' ) return false;
-
-    return (value.length > 0);
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
-function isNotEmpty(value) {
-    if (value == null || typeof value == 'undefined' ) return false;
-    return (value.length > 0);
-   }
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-function isNumber(num) {
-    return (num.length > 0) && !isNaN(num);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
-
-function isEmail(email) {
-    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return regex.test(String(email).toLowerCase());
-}
-
-function fieldValidation(field, validationFunction) {
-    if (field == null) return false;
-   
-    let isFieldValid = validationFunction(field.value)
-    if (!isFieldValid) {
-    field.className = 'placeholderRed';
-    } else {
-    field.className = '';
-    }
-   
-    return isFieldValid;
-   }
-
-if (!isFieldValid) {
-    field.className = 'placeholderRed';
-}   else {
-    field.className = '';
-}
-
-function isValid() {
-    var valid = true;
-    
-    valid &= fieldValidation(fields.firstName, isNotEmpty);
-    valid &= fieldValidation(fields.lastName, isNotEmpty);
-    valid &= fieldValidation(fields.gender, isNotEmpty);
-    valid &= fieldValidation(fields.address, isNotEmpty);
-    valid &= fieldValidation(fields.country, isNotEmpty);
-    valid &= fieldValidation(fields.email, isEmail);
-    valid &= fieldValidation(fields.houseNumber, isNumber);
-    valid &= fieldValidation(fields.password, isPasswordValid);
-    valid &= fieldValidation(fields.passwordCheck, isPasswordValid);
-    valid &= fieldValidation(fields.question, isNotEmpty);
-    valid &= arePasswordsEqual();
-   
-    return valid;
-}
-
-class User {
-    constructor(firstName, lastName, gender, address, country, email, newsletter, question) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
-    this.address = address;
-    this.country = country;
-    this.email = email;
-    this.newsletter = newsletter;
-    this.question = question;
-    }
-   }
-
-   class User {
-    constructor(firstName, lastName, gender, address, country, email, newsletter, question) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
-    this.address = address;
-    this.country = country;
-    this.email = email;
-    this.newsletter = newsletter;
-    this.question = question;
-    }
-   }
-
-
-
